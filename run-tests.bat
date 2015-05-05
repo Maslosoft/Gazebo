b61876b914025488640d6b9daa3e457479cc7671
Peter Maselkowski <pmaselkowski@gmail.com>
2015-05-05 10:34:41 +0200

    >> Move tests to separate project

--- run-tests.bat
+++ run-tests.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET BIN_TARGET=%~dp0/vendor/phpunit/phpunit/phpunit
-php "%BIN_TARGET%" --colors --bootstrap vendor/maslosoft/mangantest/bootstrap.php vendor/maslosoft/mangantest/tests
+php "%BIN_TARGET%" --colors --bootstrap vendor/maslosoft/gazebotest/bootstrap.php vendor/maslosoft/mangantest/tests
