64a9921856a7d3ac95ed16aa042f8229d953be4b
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-11 08:38:03 +0200

    - removed obsolte files

--- travis-ocular.sh
+++
@@ -1,9 +0,0 @@
-#/!bin/bash
-wget --no-check-certificate https://scrutinizer-ci.com/ocular.phar
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] && [ "$TRAVIS_PHP_VERSION" != "nightly" ];
-	if ["$@" != ''];
-		then
-	fi;
-	then
-		php ocular.phar code-coverage:upload --repository=g/$@ --revision=`git rev-parse HEAD` --format=php-clover ./tests/_output/coverage.clover;
-fi;
